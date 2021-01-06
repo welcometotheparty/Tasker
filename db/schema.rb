7695bda4c1067332d2f9dcd4a3d19fe290120eb8
Your Name <Grifon903@gmail.com>
2021-01-06 21:39:20 +0300

    rework buttons

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,15 @@
 
 ActiveRecord::Schema.define(version: 2020_10_26_074742) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
